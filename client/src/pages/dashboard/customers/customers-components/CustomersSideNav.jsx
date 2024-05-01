e8c626975ea11e4982921372b17f00cc21c52a23
Adisie Fenta <adisiefenta23@gmail.com>
2024-05-01 16:17:49 +0300

    some changes

--- client/src/pages/dashboard/customers/customers-components/CustomersSideNav.jsx
+++ client/src/pages/dashboard/customers/customers-components/CustomersSideNav.jsx
@@ -1,4 +1,5 @@
 import { NavLink } from 'react-router-dom'
+import { useState } from 'react';
 // icons
 import { HiOutlineHome } from "react-icons/hi2";
 import { BsInbox } from "react-icons/bs";
@@ -11,96 +12,151 @@ import { CiWallet } from "react-icons/ci";
 import { CiImageOn } from "react-icons/ci";
 import { PiFileAudioLight } from "react-icons/pi";
 import { CiVideoOn } from "react-icons/ci";
+import { CiChat1 } from "react-icons/ci";
+import { HiOutlineChartBarSquare } from "react-icons/hi2";
+import { HiOutlineDocumentChartBar } from "react-icons/hi2";
+import { CiMedicalCase } from "react-icons/ci";
+import { CiDollar } from "react-icons/ci";
+import { IoCloseOutline } from "react-icons/io5";
 
 import { IoSettingsOutline } from "react-icons/io5";
 import { HiArrowPathRoundedSquare } from "react-icons/hi2";
 import { AiOutlineAppstoreAdd } from "react-icons/ai";
 import { LuUsers } from "react-icons/lu";
 
+import { IoIosAddCircleOutline } from "react-icons/io";
+import { CiSquareQuestion } from "react-icons/ci";
+import { TfiLoop } from "react-icons/tfi";
+import { VscArrowSwap } from "react-icons/vsc";
+import { LuGraduationCap } from "react-icons/lu";
+import { CiRollingSuitcase } from "react-icons/ci";
+import { IoDocumentTextOutline } from "react-icons/io5";
+import { MdOutlineSupervisorAccount } from "react-icons/md";
+import { VscLayersActive } from "react-icons/vsc";
+import { HiOutlineLockClosed } from "react-icons/hi";
+import { VscNewFolder } from "react-icons/vsc";
+import { CiViewTable } from "react-icons/ci";
+
 import { MdKeyboardArrowDown } from "react-icons/md";
 
 const CustomersSideNav = () => {
+    const [activeLink, setActiveLink] = useState({
+        icon: RxDashboard,
+        title: 'Dashboard',
+        path: '#',
+    })
+    const [subLinks, setSubLinks] = useState(null)
+
     // link items
     const linkItems = [
-        {
-            icon: HiOutlineHome,
-            title: 'Home',
-            path: '#',
-        },
-        {
-            icon: BsInbox,
-            title: 'Inbox',
-            path: '#',
-        },
-        {
-            icon: CiFileOn,
-            title: 'Docs',
-            path: '#',
-        },
         {
             icon: RxDashboard,
             title: 'Dashboard',
             path: '#',
         },
         {
-            icon: PiVideoLight,
-            title: 'Clips',
+            icon: CiMedicalCase,
+            title: 'Compliance',
             path: '#',
             height: '',
             subLinkItems: [
                 {
-                    icon: CiWallet,
-                    title: 'All',
+                    icon: VscNewFolder,
+                    title: 'New Case',
                     path: '#',
                 },
                 {
-                    icon: CiImageOn,
-                    title: 'Image',
+                    icon: CiViewTable,
+                    title: 'On Request',
+                    path: '#',
+                },
+                
+            ]
+        },
+        {
+            icon: HiOutlineDocumentChartBar,
+            title: 'My Chart',
+            path: '#',
+            height: '',
+            subLinkItems: [
+                {
+                    icon: MdOutlineSupervisorAccount,
+                    title: 'Case Team',
                     path: '#',
                 },
                 {
-                    icon: PiFileAudioLight,
-                    title: 'Audio',
+                    icon: VscLayersActive,
+                    title: 'Active Case',
                     path: '#',
                 },
                 {
-                    icon: CiVideoOn,
-                    title: 'Video',
+                    icon: HiOutlineLockClosed,
+                    title: 'Closed Case',
                     path: '#',
                 },
             ]
         },
         {
-            icon: CiClock1,
-            title: 'Timesheet',
+            icon: CiChat1,
+            title: 'Message',
             path: '#',
         },
         {
-            icon: CiCircleMore,
-            title: 'More',
+            icon: CiClock1,
+            title: 'Appointment',
+            path: '#',
+
+        },
+        {
+            icon: CiDollar,
+            title: 'Finance',
             path: '#',
             height: '',
             subLinkItems: [
                 {
-                    icon: IoSettingsOutline,
-                    title: 'Setting',
+                    icon: IoIosAddCircleOutline,
+                    title: 'Add Funds',
                     path: '#',
                 },
                 {
-                    icon: HiArrowPathRoundedSquare,
-                    title: 'Upgrade',
+                    icon: CiSquareQuestion,
+                    title: 'Requested Payment',
                     path: '#',
                 },
                 {
-                    icon: AiOutlineAppstoreAdd,
-                    title: 'Apps',
+                    icon: TfiLoop,
+                    title: 'Refund Funds',
                     path: '#',
                 },
                 {
-                    icon: LuUsers,
-                    title: 'users',
+                    icon: VscArrowSwap,
+                    title: 'Recent Transactions',
                     path: '#',
                 },
+            ]
+        },
+        {
+            icon: CiCircleMore,
+            title: 'Other Services',
+            path: '#',
+            height: '',
+            subLinkItems: [
+                {
+                    icon: LuGraduationCap,
+                    title: 'Training',
+                    path: '#',
+                },
+                {
+                    icon: CiRollingSuitcase,
+                    title: 'Consulting',
+                    path: '#',
+                },
+                {
+                    icon: IoDocumentTextOutline,
+                    title: 'Research',
+                    path: '#',
+                },
+                
             ]
         },
     ]
@@ -121,9 +177,16 @@ const CustomersSideNav = () => {
                             {
                                 linkItems.map((linkItem, index) => {
                                     return (
-                                        <li key={index}>
+                                        <li key={index} className='relative'>
                                             <div key={index} className='flex items-center mb-3'>
-                                                <NavLink className={'w-full flex items-center justify-between p-1 rounded-sm bg-transparent transition-colors ease-in-out duration-300 hover:bg-gray-200'}>
+                                                <NavLink className={`w-full flex items-center justify-between p-1 rounded-sm  transition-colors ease-in-out duration-300  ${activeLink?.title === linkItem.title ? 'bg-blue-200' : 'bg-transparent hover:bg-gray-200'}`} onClick={() => {
+                                                    setActiveLink(linkItem)
+                                                    if (activeLink?.title === linkItem.title && subLinks) {
+                                                        setSubLinks(null)
+                                                    } else {
+                                                        setSubLinks(linkItem.subLinkItems)
+                                                    }
+                                                }}>
                                                     <div className='flex items-center gap-3'>
                                                         <div>
                                                             <linkItem.icon className='text-xl' />
@@ -135,7 +198,7 @@ const CustomersSideNav = () => {
                                                     <div>
                                                         {
                                                             linkItem.subLinkItems ?
-                                                                <MdKeyboardArrowDown className='text-lg' />
+                                                                <IoCloseOutline className={`text-sm transition-transform ease-in-out duration-300 ${activeLink?.title === linkItem.title && subLinks ? 'rotate-0' : '-rotate-45'}`} />
                                                                 :
                                                                 <></>
                                                         }
@@ -144,16 +207,29 @@ const CustomersSideNav = () => {
                                             </div>
                                             {/* sub list */}
                                             {
-                                                linkItem.subLinkItems && false
+                                                linkItem.subLinkItems
                                                     ?
-                                                    <div>
+                                                    <div className={`absolute left-[108%] bg-gray-100 whitespace-nowrap shadow-lg top-1/2 w-[200px] border border-gray-300 -translate-y-1/2 z-50 p-2 transition-transform ease-in-out duration-300 ${activeLink?.title === linkItem.title && subLinks ? 'scale-100' : 'scale-0'}`}>
                                                         <ul>
-                                                            <li>
-                                                                <NavLink>
-                                                                    <div>Left</div>
-                                                                </NavLink>
-                                                            </li>
+                                                            {
+                                                                linkItem.subLinkItems?.map((subLinkItem, index) => {
+                                                                    return (
+                                                                        <li key={index} className='flex items-center my-1'>
+                                                                            <NavLink className={'w-full bg-transparent p-1 transition-colors ease-in-out duration-300 hover:bg-gray-200'}>
+                                                                                <div className='flex items-center gap-3'>
+                                                                                    <div>
+                                                                                        <subLinkItem.icon className='text-lg' />
+                                                                                    </div>
+                                                                                    <div><span>{subLinkItem.title}</span></div>
+                                                                                </div>
+                                                                            </NavLink>
+                                                                        </li>
+                                                                    )
+                                                                })
+                                                            }
                                                         </ul>
+                                                        {/* icon */}
+                                                        <div className='absolute left-0 top-1/2 -translate-y-1/2 w-[18px] rotate-45 ml-[-9px] aspect-square bg-inherit border-b border-l border-gray-300'></div>
                                                     </div>
                                                     :
                                                     <></>
