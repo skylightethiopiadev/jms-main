2de7261a6379d20172a71646bee084b99bbbde97
Adisie Fenta <adisiefenta23@gmail.com>
2024-05-09 10:12:56 +0300

    chnages on the customers dashboard

--- client/src/pages/dashboard/customers/customers-pages/CustomersDashboardIndex.jsx
+++ client/src/pages/dashboard/customers/customers-pages/CustomersDashboardIndex.jsx
@@ -16,6 +16,7 @@ import { GiAlarmClock } from "react-icons/gi";
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { MdOutlineChevronRight } from "react-icons/md";
 import { MdOutlineChevronLeft } from "react-icons/md";
+import { GoArrowUpRight } from "react-icons/go";
 
 const CustomersDashboardIndex = () => {
 
@@ -25,11 +26,46 @@ const CustomersDashboardIndex = () => {
 
     // case history slider settings
     const caseHistorySliderSettings = {
-        dots: true,
+        dots: false,
         infinite: true,
         speed: 500,
         slidesToShow: 4,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    }
+
+
+    // functions
+    // prev
+    const showPreviousCaseHistory = () => {
+        caseHistorySliderRef?.slickPrev()
+    }
+    // next
+    const showNextCaseHistory = () => {
+        caseHistorySliderRef?.slickNext()
     }
 
     return (
@@ -116,12 +152,15 @@ const CustomersDashboardIndex = () => {
                         </div>
                         <div></div>
                     </header>
-                    <div className='h-[85%] w-full p-1 bg-gray-100k relative rounded-md'>
-                        <div className='pb-3'>
-                            <div>
-                                <span>Case: </span>
-                                <span className='font-semibold'>Criminal</span>
+                    <div className='h-[85%] w-full p-1 relative rounded-md mt-2'>
+                        {/* bg */}
+                        <div className='absolute left-0 top-0 w-full h-full z-0'>
+                            <div className='w-full h-full overflow-hidden rounded-md bg-gradient-to-r from-white to-green-200'>
+                                
                             </div>
+                        </div>
+                        <div className='p-1 relative z-10 bg-white w-max pr-7 rounded-sm shadow-2xl'>
+                            
                             <div className='my-1'>
                                 <span>Type: </span>
                                 <span className='font-semibold'>Oral Litigation</span>
@@ -183,7 +222,7 @@ const CustomersDashboardIndex = () => {
                                     <div>t</div>
                                 </div>
                                 <div>
-                                    <div className='bg-emerald-700 text-white flex items-center justify-center rounded-full overflow-hidden p-[.1rem] w-[20px] aspect-square'><span className='text-sm font-medium'>12</span></div>
+                                    <div className='bg-red-600 text-white flex items-center justify-center rounded-full overflow-hidden p-[.1rem] w-[20px] aspect-square'><span className='text-sm font-medium'>12</span></div>
                                 </div>
                             </div>
 
@@ -240,14 +279,14 @@ const CustomersDashboardIndex = () => {
                 <div className="border border-gray-200 rounded-md px-7 py-5">
                     <header className="flex items-center justify-between">
                         <div>
-                            <h3 className="header-level-4">Balance</h3>
+                            <h3 className="header-level-4">Current Balance</h3>
                         </div>
                         <div></div>
                     </header>
-                    <div>
+                    <div className='mt-1 p-2 rounded-md bg-gradient-to-r from-white to-orange-200'>
                         <header className='flex items-center justify-between'>
                             <div>
-                                <h3 className='font-medium'>Expense Analysis</h3>
+                                <h3 className='font-medium'></h3>
                             </div>
                             <div>
                                 <div className='flex items-center gap-1 cursor-pointer border border-gray-100 px-2 rounded-full transition-colors ease-in-out duration-150 hover:border-gray-300'>
@@ -259,38 +298,17 @@ const CustomersDashboardIndex = () => {
                             </div>
                         </header>
                         <div className='text-sm'>
-                            <div className='flex items-center gap-2'>
-                                <div className='flex items-center w-[115px]'><span>current balance</span></div>
-                                <div className='w-[90px] h-[4px] bg-transparent rounded-full'>
-                                    <div className='w-full h-full rounded-full bg-green-400'></div>
-                                </div>
-                                <div className='flex items-center font-medium'><span>$21,328</span></div>
+                            <h1 className='text-xl font-bold'>$21,328.00</h1>
+                            <div className='mt-[-2px]'>
+                                <h3 className='text-gray-700  font-semibold'>left to spend</h3>
                             </div>
-                            <div className='flex items-center gap-2'>
-                                <div className='flex items-center w-[115px]'><span>total spent</span></div>
-                                <div className='w-[90px] h-[4px] bg-transparent rounded-full'>
-                                    <div className='h-full w-[65%] rounded-full bg-red-500'></div>
-                                </div>
-                                <div className='flex items-center font-medium'><span>$21,328</span></div>
-                            </div>
-                            <div className='flex items-center gap-2'>
-                                <div className='flex items-center w-[115px]'><span>utility</span></div>
-                                <div className='w-[90px] h-[4px] bg-transparent rounded-full'>
-                                    <div className='w-[85%] h-full bg-yellow-400 rounded-full'></div>
-                                </div>
-                                <div className='flex items-center font-medium'><span>$21,328</span></div>
-                            </div>
-                            <div className='flex items-center gap-2'>
-                                <div className='flex items-center w-[115px]'><span>total refund</span></div>
-                                <div className='w-[90px] h-[4px] bg-transparent rounded-full'>
-                                    <div className='w-[50%] h-full rounded-full bg-blue-600'></div>
-                                </div>
-                                <div className='flex items-center font-medium'><span>$21,328</span></div>
+                            <div className='mt-[-2px]'>
+                                <span className='text-xs'>last update 2 months ago</span>
                             </div>
                         </div>
-                        <div className='mt-2 flex items-center gap-3'>
-                            <button className='px-1 py-[.1rem] rounded-sm bg-yellow-400 text-white transition-colors ease-in-out duration-150 hover:bg-yellow-300'>Add fund</button>
-                            <button className='px-1 py-[.1rem] rounded-sm bg-yellow-400 text-white transition-colors ease-in-out duration-150 hover:bg-yellow-300'>Withdraw funds</button>
+                        <div className='mt-2 flex items-center gap-5'>
+                            <button className='px-3 py-[.15rem] rounded-full border border-yellow-400 transition-colors ease-in-out duration-300 hover:bg-yellow-400 hover:text-white'>Add fund</button>
+                            <button className='px-3 py-[.15rem] rounded-full border border-yellow-400 transition-colors ease-in-out duration-300 hover:bg-yellow-400 hover:text-white'>Withdraw funds</button>
                         </div>
                     </div>
                 </div>
@@ -304,28 +322,63 @@ const CustomersDashboardIndex = () => {
                         <h3 className="header-level-4 whitespace-nowrap">Case History</h3>
                     </div>
                     <div className='flex items-center justify-end gap-3'>
-                        <div className='w-[20px] aspect-square border border-blue-900 rounded-full overflow-hidden flex items-center justify-center text-blue-900 text-lg cursor-pointer transition-colors ease-in-out duration-150 hover:bg-blue-900 hover:text-white'>
+                        <div className='w-[20px] aspect-square border border-blue-900 rounded-full overflow-hidden flex items-center justify-center text-blue-900 text-lg cursor-pointer transition-colors ease-in-out duration-150 hover:bg-blue-900 hover:text-white' onClick={() => {
+                            showPreviousCaseHistory()
+                        }}>
                             <MdOutlineChevronLeft />
                         </div>
-                        <div className='w-[20px] aspect-square border border-blue-900 rounded-full overflow-hidden flex items-center justify-center text-blue-900 text-lg cursor-pointer transition-colors ease-in-out duration-150 hover:bg-blue-900 hover:text-white'>
+                        <div className='w-[20px] aspect-square border border-blue-900 rounded-full overflow-hidden flex items-center justify-center text-blue-900 text-lg cursor-pointer transition-colors ease-in-out duration-150 hover:bg-blue-900 hover:text-white' onClick={() => {
+                            showNextCaseHistory()
+                        }}>
                             <MdOutlineChevronRight />
                         </div>
                     </div>
                 </header>
-                <div className="mt-4 h-[210px] max-w-[1133.86px] overflow-y-hidde overflow-x-hidden flex gap-5" id='www' onClick={() => {
-                    console.log(window.getComputedStyle(document.getElementById('www')).width)
-                }}>
+                <div className="mt-4 h-[110px] overflow-y-hidden max-w-[1133.86px] overflow-x-hidden flex gap-5">
                     <div className='w-full'>
-                        <Slider {...caseHistorySliderSettings}>
+                        <Slider ref={slider => {
+                            caseHistorySliderRef = slider;
+                        }} {...caseHistorySliderSettings}>
                             {
                                 [...Array(12)].map((item, index) => {
                                     return (
-                                        <div key={index} className='px-3'>
-                                            <div>
-                                                <h3>One</h3>
-                                                <p>
-                                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae dolore laborum quaerat, voluptas, ea, officia non obcaecati perspiciatis aperiam maiores ullam earum molestias optio?
-                                                </p>
+                                        <div key={index} className='px-3 pb-5'>
+                                            <div className={`px-2 py-3 rounded-md bg-white shadow-md border-l-[3px] ${index % 3 === 0 ? 'border-green-400' : index % 2 === 0 ? 'border-yellow-400' : 'border-red-500'}`}>
+                                                <header className='flex items-center justify-between'>
+                                                    <div>
+                                                        <span className='text-gray-700 font-medium text-sm'>No.732164a</span>
+                                                    </div>
+                                                    <div>
+                                                        <div className='w-[24px] rounded-sm border border-gray-200 flex items-center justify-center text-gray-600 cursor-pointer transition-colors ease-in-out duration-150 hover:border-gray-400 hover:text-black'>
+                                                            <GoArrowUpRight />
+                                                        </div>
+                                                    </div>
+                                                </header>
+                                                <div>
+                                                    <h3 className='font-bold'>Ledeta Kefele Ketema</h3>
+                                                </div>
+                                                <div>
+                                                    <h4 className='font-semibold text-sm text-gray-700'>fetaber chelot</h4>
+                                                </div>
+                                                <div className='flex items-center justify-between'>
+                                                    <div>
+                                                        <span className='text-xs'>starts 5 days ago</span>
+                                                    </div>
+                                                    <div>
+                                                        <div className='relative flex items-center justify-end cursor-pointer'>
+                                                            <div className='absolute right-7 z-0 w-[24px] aspect-square rounded-full overflow-hidden bg-red-500'>
+                                                                <img className='w-full h-full object-center object-cover' src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg" alt="" />
+                                                            </div>
+                                                            <div className='absolute right-4 z-10 w-[24px] aspect-square rounded-full overflow-hidden bg-green-500'>
+                                                                <img className='w-full h-full object-center object-cover' src="https://img.freepik.com/free-photo/cheerful-dark-skinned-woman-smiling-broadly-rejoicing-her-victory-competition-among-young-writers-standing-isolated-against-grey-wall-people-success-youth-happiness-concept_273609-1246.jpg" alt="" />
+                                                            </div>
+                                                            <div className='absolute right-0 z-20 w-[26px] aspect-square rounded-full overflow-hidden bg-white shadow-md border border-white flex items-center justify-center'>
+                                                                <span className='text-xs font-bold'>5+</span>
+                                                            </div>
+
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                     )
